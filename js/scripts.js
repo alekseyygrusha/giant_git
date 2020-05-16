17e0aff2efd48f3e029a3c1f753f4c4f6ed87f01
Alex <alekseyy-grusha@rambler.ru>
2020-05-16 15:53:36 +0300

    1
    1

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,39 @@ $(document).ready(function() {
             $('.price_box').toggleClass('.gradient_background')
         }
     );
+
+
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+        ]
+    });
+
 });
 
 (function($) {
@@ -64,6 +97,9 @@ $(document).ready(function() {
         });
     });
 })(jQuery);
+
+
+
 
 
 
